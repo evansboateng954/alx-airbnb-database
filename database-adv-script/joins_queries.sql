d9d6513ea004ce82cf00dd76a5eea6c687a91282
evansboateng954 <87736085+evansboateng954@users.noreply.github.com>
2025-09-06 01:44:21 -0700

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,11 @@
+-- Selecting users who made booking(s)
+SELECT b.*, u.* FROM booking b INNER JOIN user u ON b.user_id = u.user_id ;
+
+
+--selecting all properties and thier reviews 
+-- get properties without review as well
+SELECT p.*, r.* FROM property p LEFT JOIN review r ON p.property_id = r.property_id;
+
+
+--select all elements from both user and booking table
+SELECT u.*, b.* FROM user u FULL OUTER JOIN  booking b ON u.user_id = b.user_id;
